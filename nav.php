631508df7108c0899be2759f633b666c077d41dd
jjssoolee <jjssoolee@gmail.com>
2017-06-10 14:20:00 +0900

    initial commit

---
+++ nav.php
@@ -0,0 +1,77 @@
+<nav style="margin-bottom: 5px; font-size:14px;">
+  <ul class="nav nav-pills nav-justified">
+    <li role="presentation" class="dropdown" ><a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false" style="padding-top: 5px; padding-bottom: 5px;">
+      기능성화장품<span class="caret"></span></a>
+      <ul class="dropdown-menu" style="width: 280px;">
+        <li><a href="showcategory.php?value=링클케어">링클케어</a></li><li><a href="showcategory.php?value=모공케어">모공케어</a></li><li><a href="showcategory.php?value=아이케어">아이케어</a></li><li><a href="showcategory.php?value=트러블케어">트러블케어</a></li><li><a href="showcategory.php?value=화이트닝케어">화이트닝케어</a></li>
+      </ul></li>
+      <li role="presentation" class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false" style="padding-top: 5px; padding-bottom: 5px;">
+        네일케어<span class="caret"></span></a>
+        <ul class="dropdown-menu" style="width: 280px;">
+        <li><a href="showcategory.php?value=네일리무버">네일리무버</a></li><li><a href="showcategory.php?value=네일아트">네일아트</a></li><li><a href="showcategory.php?value=네일영양제">네일영양제</a></li><li><a href="showcategory.php?value=네일컬러매니큐어">네일컬러매니큐어</a></li><li><a href="showcategory.php?value=네일케어도구">네일케어도구</a></li><li><a href="showcategory.php?value=베이스코트">베이스코트</a></li><li><a href="showcategory.php?value=탑코트">탑코트</a></li>
+      </ul></li>
+      <li role="presentation" class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false" style="padding-top: 5px; padding-bottom: 5px;">
+        마스크팩<span class="caret"></span></a>
+        <ul class="dropdown-menu" style="width: 280px;">
+        <li><a href="showcategory.php?value=마사지크림젤">마사지크림젤</a></li><li><a href="showcategory.php?value=마스크시트">마스크시트</a></li><li><a href="showcategory.php?value=수딩젤팩">수딩젤팩</a></li><li><a href="showcategory.php?value=슬리핑팩수면팩">슬리핑팩수면팩</a></li><li><a href="showcategory.php?value=워시오프팩">워시오프팩</a></li><li><a href="showcategory.php?value=코팩">코팩</a></li><li><a href="showcategory.php?value=필오프팩모델링팩">필오프팩모델링팩</a></li>
+      </ul></li>
+      <li role="presentation" class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false" style="padding-top: 5px; padding-bottom: 5px;">
+        바디케어<span class="caret"></span></a>
+        <ul class="dropdown-menu" style="width: 280px;">
+        <li><a href="showcategory.php?value=데오도란트">데오도란트</a></li><li><a href="showcategory.php?value=목욕비누">목욕비누</a></li><li><a href="showcategory.php?value=바디로션">바디로션</a></li><li><a href="showcategory.php?value=바디미스트샤워코롱">바디미스트샤워코롱</a></li><li><a href="showcategory.php?value=바디밤파우더">바디밤파우더</a></li><li><a href="showcategory.php?value=바디스크럽">바디스크럽</a></li><li><a href="showcategory.php?value=바디오일에센스">바디오일에센스</a></li><li><a href="showcategory.php?value=바디워시클렌저">바디워시클렌저</a></li><li><a href="showcategory.php?value=바디크림">바디크림</a></li><li><a href="showcategory.php?value=여성청결제">여성청결제</a></li><li><a href="showcategory.php?value=왁싱제모">왁싱제모</a></li><li><a href="showcategory.php?value=입욕제">입욕제</a></li><li><a href="showcategory.php?value=핸드케어">핸드케어</a></li>
+      </ul></li>
+      <li role="presentation" class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false" style="padding-top: 5px; padding-bottom: 5px;">
+        베이스메이크업<span class="caret"></span></a>
+        <ul class="dropdown-menu" style="width: 280px;">
+        <li><a href="showcategory.php?value=BB크림">BB크림</a></li><li><a href="showcategory.php?value=CC크림">CC크림</a></li><li><a href="showcategory.php?value=메이크업베이스">메이크업베이스</a></li><li><a href="showcategory.php?value=컨실러">컨실러</a></li><li><a href="showcategory.php?value=쿠션">쿠션</a></li><li><a href="showcategory.php?value=파우더팩트">파우더팩트</a></li><li><a href="showcategory.php?value=파운데이션">파운데이션</a></li><li><a href="showcategory.php?value=프라이머">프라이머</a></li>
+      </ul></li>
+      <li role="presentation" class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false" style="padding-top: 5px; padding-bottom: 5px;">
+        뷰티소품<span class="caret"></span></a>
+        <ul class="dropdown-menu" style="width: 280px;">
+        <li><a href="showcategory.php?value=메이크업브러쉬">메이크업브러쉬</a></li><li><a href="showcategory.php?value=바디소품">바디소품</a></li><li><a href="showcategory.php?value=아이소품">아이소품</a></li><li><a href="showcategory.php?value=페이스소품">페이스소품</a></li><li><a href="showcategory.php?value=헤어소품">헤어소품</a></li>
+      </ul></li>
+      <li role="presentation" class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false" style="padding-top: 5px; padding-bottom: 5px;">
+        색조메이크업<span class="caret"></span></a>
+        <ul class="dropdown-menu" style="width: 280px;">
+        <li><a href="showcategory.php?value=립글로스립라커">립글로스립라커</a></li><li><a href="showcategory.php?value=립라이너">립라이너</a></li><li><a href="showcategory.php?value=립밤">립밤</a></li><li><a href="showcategory.php?value=립스틱">립스틱</a></li><li><a href="showcategory.php?value=립틴트">립틴트</a></li><li><a href="showcategory.php?value=마스카라">마스카라</a></li><li><a href="showcategory.php?value=블러셔">블러셔</a></li><li><a href="showcategory.php?value=아이라이너">아이라이너</a></li><li><a href="showcategory.php?value=아이브로우">아이브로우</a></li><li><a href="showcategory.php?value=아이섀도우">아이섀도우</a></li><li><a href="showcategory.php?value=픽서영양제">픽서영양제</a></li><li><a href="showcategory.php?value=하이라이터쉐딩">하이라이터쉐딩</a></li>
+      </ul></li>
+    </ul>
+    </nav>
+    <nav style="margin-bottom: 50px; font-size:14px;">
+      <ul class="nav nav-pills nav-justified">
+      <li role="presentation" class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false" style="padding-top: 5px; padding-bottom: 5px;">
+        선케어<span class="caret"></span></a>
+        <ul class="dropdown-menu" style="width: 280px;">
+        <li><a href="showcategory.php?value=선크림">선크림</a></li>
+      </ul></li>
+      <li role="presentation" class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false" style="padding-top: 5px; padding-bottom: 5px;">
+        스킨케어<span class="caret"></span></a>
+        <ul class="dropdown-menu" style="width: 280px;">
+        <li><a href="showcategory.php?value=로션에멀전">로션에멀전</a></li><li><a href="showcategory.php?value=미스트픽서">미스트픽서</a></li><li><a href="showcategory.php?value=스킨토너">스킨토너</a></li><li><a href="showcategory.php?value=에센스세럼앰플">에센스세럼앰플</a></li><li><a href="showcategory.php?value=크림">크림</a></li><li><a href="showcategory.php?value=페이스오일">페이스오일</a></li>
+      </ul></li>
+      <li role="presentation" class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false" style="padding-top: 5px; padding-bottom: 5px;">
+        클렌징<span class="caret"></span></a>
+        <ul class="dropdown-menu" style="width: 280px;">
+        <li><a href="showcategory.php?value=스크럽필링">스크럽필링</a></li><li><a href="showcategory.php?value=클렌징로션밀크">클렌징로션밀크</a></li><li><a href="showcategory.php?value=클렌징비누">클렌징비누</a></li><li><a href="showcategory.php?value=클렌징오일">클렌징오일</a></li><li><a href="showcategory.php?value=클렌징워터">클렌징워터</a></li><li><a href="showcategory.php?value=클렌징크림">클렌징크림</a></li><li><a href="showcategory.php?value=클렌징티슈">클렌징티슈</a></li><li><a href="showcategory.php?value=클렌징폼젤">클렌징폼젤</a></li><li><a href="showcategory.php?value=포인트리무버">포인트리무버</a></li>
+      </ul></li>
+      <li role="presentation" class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false" style="padding-top: 5px; padding-bottom: 5px;">
+        향수<span class="caret"></span></a>
+        <ul class="dropdown-menu" style="width: 280px;">
+        <li><a href="showcategory.php?value=남성향수">남성향수</a></li><li><a href="showcategory.php?value=남여공용향수">남여공용향수</a></li><li><a href="showcategory.php?value=섬유향수패브릭향수드래스퍼퓸">섬유향수패브릭향수드래스퍼퓸</a></li><li><a href="showcategory.php?value=여성향수">여성향수</a></li><li><a href="showcategory.php?value=향초디퓨저방향제">향초디퓨저방향제</a></li>
+      </ul></li>
+      <li role="presentation" class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false" style="padding-top: 5px; padding-bottom: 5px;">
+        헤어스타일링<span class="caret"></span></a>
+        <ul class="dropdown-menu" style="width: 280px;">
+        <li><a href="showcategory.php?value=염색제파마제">염색제파마제</a></li><li><a href="showcategory.php?value=헤어왁스스프레이젤">헤어왁스스프레이젤</a></li>
+      </ul></li>
+      <li role="presentation" class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false" style="padding-top: 5px; padding-bottom: 5px;">
+        헤어케어<span class="caret"></span></a>
+        <ul class="dropdown-menu" style="width: 280px;">
+        <li><a href="showcategory.php?value=두피케어">두피케어</a></li><li><a href="showcategory.php?value=린스컨디셔너">린스컨디셔너</a></li><li><a href="showcategory.php?value=샴푸">샴푸</a></li><li><a href="showcategory.php?value=트리트먼트">트리트먼트</a></li><li><a href="showcategory.php?value=헤어미스트퍼퓸">헤어미스트퍼퓸</a></li><li><a href="showcategory.php?value=헤어에센스">헤어에센스</a></li><li><a href="showcategory.php?value=헤어팩">헤어팩</a></li>
+      </ul></li>
+      <li role="presentation" class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false" style="padding-top: 5px; padding-bottom: 5px;">
+        기타<span class="caret"></span></a>
+        <ul class="dropdown-menu" style="width: 280px;">
+        <li><a href="showcategory.php?value=건강다이어트식품">건강다이어트식품</a></li>
+      </ul></li>
+      </ul></nav>
---
+++ search.php
@@ -0,0 +1,105 @@
+<?php
+$value=$_POST["value"];
+ ?>
+<?php
+require("config/config.php");
+require("lib/db.php");
+$conn = db_init($config["host"], $config["duser"], $config["dpw"], $config["dname"]);
+    ?>
+    <?php
+    $useragent=$_SERVER['HTTP_USER_AGENT'];
+    if(preg_match('/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i',$useragent)||preg_match('/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i',substr($useragent,0,4))){
+      $margin='10px';
+    } else {
+      $margin='100px';
+    }
+    ?>
+<html style="padding-right: <?php echo $margin?>; padding-left: <?php echo $margin?>;">
+  <head>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
+
+<style type="text/css">
+a:link {text-decoration: none; color: #333333;}
+a:visited {text-decoration: none; color: #333333;}
+a:active {text-decoration: none; color: #333333;}
+a:hover {text-decoration: underline; color: red;}
+html { font-size: 62.5%; }
+body { font-size: 1em;}
+
+
+</style>
+    <title>
+      Dongguk Cosmetic
+    </title>
+
+    <link href="http://jjssoolee.cafe24.com/jkjs/bootstrap-3.3.6-dist/css/bootstrap.min.css" rel="stylesheet">
+        <style>
+          .nav>li>a:focus,.nav>li>a:hover{text-decoration:none;background-color:#FFC6C3;}
+          .nav .open>a,.nav .open>a:focus,.nav .open>a:hover{background-color:#FFC6C3;border-color:#337ab7}
+          .dropdown-menu>li>a:focus,.dropdown-menu>li>a:hover{color:#262626;text-decoration:none;background-color:#FFC6C3;}
+        </style>
+      </head>
+      <header class="jumbotron text-center" style="background-color: #FFC6B3; padding-bottom:10px;">
+        <a href="index.php"><h1>Dongguk Cosmetic</h1></a>
+
+          <div class"col-xs-6 col-sm-3"></div>
+            <div class"col-xs-6 col-sm-6">
+              <form action="search.php" method="post">
+                <div class="input-group" style="text-align:center; margin-bottom:50px; margin-top:50px">
+                  <input type="text" name="value" placeholder="Search for..." class="form-control" style="vertical-align:middle " >
+                  <span class="input-group-btn">
+              <button type="submit" class="btn btn-danger">SEARCH</button>
+            </span>
+            </div>
+            </form>
+          </div>
+          <div class"col-xs-6 col-sm-3"></div>
+      </header>
+      <?php include("nav.php");?>
+      <body class="container-fluid" style="margin-bottom: 20px;margin-left: 5px;margin-right: 5px; border:1px solid red; border-bottom-left-radius: 10px; border-bottom-right-radius: 10px; border-top-left-radius: 10px; border-top-right-radius: 10px; padding-left: 0;padding-right:0">
+            <article style="text-align:center;">
+              <?php
+              echo "<table class=\"table table-striped\" style=\"font-size:12px;\">";
+              echo "<tr><th style=\"font-weight:normal; width: 50px; text-align:center;\">";
+              echo "순위";
+              echo "</th><th style=\"font-weight:normal; width: 100px; text-align:center;\">";
+              echo "제품 사진";
+              echo "</th><th style=\"font-weight:normal; width: 300px; text-align:center;\">";
+              echo "제품명";
+              echo "</th><th style=\"font-weight:normal; width: 20px; text-align:center;\">";
+              echo "점수";
+              echo "</th></tr>";
+              $sql = "SELECT*FROM `bigdata` WHERE `product` LIKE '%".$value."%' ORDER BY `score` DESC";
+
+              $result = mysqli_query($conn, $sql);
+
+              $i = 1;
+              while ($row= mysqli_fetch_assoc($result)){
+
+                if($row['reviewnum']>10){
+
+                  echo "<tr OnClick=\"location.href='showproduct.php?value=".$row['product']."'\" style=\"cursor:pointer;\"><th style=\"font-size:16px; font-weight:normal; width: 50px; text-align:center; vertical-align:middle;\">";
+                  echo $i;
+                  echo "</th><th style=\"font-size:16px; font-weight:normal; width: 100px; text-align:center; vertical-align:middle;\">";
+                  echo "<img src=".$row['img']." style=\"width:80px\">";
+                  echo "</th><th style=\"font-size:16px; font-weight:normal; width: 300px; text-align:center; vertical-align:middle;\">";
+                  echo "<a href=\"showproduct.php?value=".$row['product']."\">";
+                  echo htmlspecialchars($row['product']);
+                  echo "</a>";
+                  echo "</th><th style=\"font-size:16px; font-weight:normal; width: 20px; text-align:center; vertical-align:middle;\">";
+                  echo nl2br(strip_tags($row['score'], '<a><h1><h2><h3><h4><h5><ul><ol><li>'));
+                  echo "</th></tr>";
+                  $i += 1;
+                }
+              }
+              ?>
+              </table>
+
+
+            </article>
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+            <script src="http://jjssoolee.cafe24.com/jkjs/bootstrap-3.3.6-dist/js/bootstrap.min.js"></script>
+          </body>
+        </html>
